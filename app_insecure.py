0082009301c6eb9c76e1bdddc92bc746aeea2f27
Karan Erry <kerry@drew.edu>
2018-01-21 21:19:58 -0500

    fixing requirements?

--- app_insecure.py
+++ app_insecure.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request
 from datetime import time
 from flask_uploads import UploadSet, configure_uploads, IMAGES, ALL
 from pyexcel_xlsx import get_data
@@ -7,56 +7,45 @@ import json
 from analyzeDoc import *
 import math, os
 
-from flask import Flask, render_template, session, redirect, request, url_for
-from googleapiclient.discovery import build
-import google_auth_oauthlib.flow, google.oauth2.credentials, oauth2client
-import requests
-import psycopg2
-import os
-
-
 
 app=Flask(__name__)
 photos = UploadSet('photos', ALL)
 
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
 configure_uploads(app, photos)
-app.secret_key = 'Random value'
 
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# Set global variables
+roomAvailList = None
 
 @app.route("/")
 def index():
-        # TEST binClasses() function:
-    # classList = [{'size':8, 'classPrefs':[1, 2]}, {'size':10, 'classPrefs':[1, 2]}, {'size':5, 'classPrefs':[3,4,5]}, {'size':6, 'classPrefs':[1, 2]}]
-    # print(binClasses(classList))
-        # TEST buildRoomAvailList() function:
-    # roomAvailList = buildRoomAvailList(['hello', 'hey', 'hi'])
-    # print(roomAvailList)
-        # TEST blockRoom() function:
-    # roomAvailList = blockRoom (roomAvailList, 'hello', 2, time(12, 25), time(13, 30))
-    # print(roomAvailList)
-    # print(blockRoom(roomAvailList, 'hello', 2, time(13, 00), time(14,00)))
+
+
     return render_template("index.html")
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST' and 'photo' in request.files:
-        global filename
         filename = photos.save(request.files['photo'])
-        #return show(filename)
-        return(render_template('success.html'))
+        return show(filename)
     return render_template('upload.html')
+
+def show(filename):
+    filePosition= "static/img/"+filename
+
+    data = get_data(filePosition)
+    s1=json.dumps(data)
+    # d2=json.loads(s1)
+    # d2= d2["Summary"]
+    # s2=json.dumps(d2)
+    return s1
+
 
 @app.route('/analyze', methods=['GET','POST'])
 def analyze():
     # Import data from spreadsheet
-    classesStuff = parseCourseDetails(parseRooms(),filename)
+    classesStuff = parseCourseDetails(parseRooms())
     classList = classesStuff['classes']
     invalidClasses = classesStuff['invalidClasses']
 
@@ -107,20 +96,6 @@ def analyze():
                     unscheduledClasses.append(classes)
             count+=1
 
-    ''' for each class in unscheduledClasses,
-            (1) loop through the rooms list
-                    (a) if size fits, check if blockroom() works
-            if can't find any spot, add to fatal failures '''
-
-    # for classes in unscheduledClasses:
-    #     for day in classes['days']:
-    #         roomIter = iter(parseRooms())
-    #         foundRoom
-    #         while not blockRoom(classes['className'], next(roomIter), day, classes['startTime'], classes['endTime']):
-    #             pass
-
-
-
     # fatalFailures = []
     #
     # for classes in unscheduledClasses:
@@ -165,8 +140,13 @@ def binClasses(classList):
     # order of number of classroom preferences specified
     binTWO = [classDetail for classDetail in classList if classDetail not in binONE]
     binTWO = sorted(binTWO, key=itemgetter('size'), reverse=True)#lambda classDetail: len(classDetail['roomPrefs']))
+    # binTWO = sorted(binTWO, key=lambda classDetail: len(classDetail['roomPrefs']))
     # Return two-item list containing bin one and bin two
     return [binONE, binTWO]
+
+
+
+
 
 def buildRoomAvailList(roomList):
     ''' Dict with roomName as key containing:
@@ -240,125 +220,6 @@ def features(roomID):
         # TODO : (returns int)
 
     # Convert roomFeaturesCode to binary
-
-
-# Login landing page
-@app.route('/identity')
-def identity():
-    return render_template('identityLanding.html')
-
-# Process OAuth authorization
-@app.route('/identity/login')
-def login():
-    if 'credentials' not in session:
-        # No user session is active
-        return redirect(url_for('authorize'))
-    try:
-        # Load credentials from the session:
-        credentials = google.oauth2.credentials.Credentials(**session['credentials'])
-        # Build the service object for the Google OAuth v2 API:
-        oauth = build('oauth2', 'v2', credentials=credentials)
-        # Call methods on the service object to return a response with the user's info:
-        userinfo = oauth.userinfo().get().execute()
-        print(userinfo)
-    except google.auth.exceptions.RefreshError:
-        # Credentials are stale
-        return redirect(url_for('authorize'))
-
-    # Verify that the user signed in with a 'drew.ed' email address:
-    if 'hd' in userinfo: validDomain = userinfo['hd'] == 'drew.edu'
-    else:                validDomain = False
-    if not validDomain:
-        return redirect(url_for('domainInvalid'))
-
-
-
-    username = userinfo['email'][:userinfo['email'].index('@')]
-
-    print(username)
-    return render_template("upload.html")
-
-
-@app.route('/identity/logout')
-def logout():
-    if 'credentials' in session:
-        # Load the credentials from the session:
-        credentials = google.oauth2.credentials.Credentials(**session['credentials'])
-        # Request the auth server to revoke the specified credentials:
-        requests.post('https://accounts.google.com/o/oauth2/revoke',
-            params={'token': credentials.token},
-            headers = {'content-type': 'application/x-www-form-urlencoded'})
-        # Delete the credentials from the session cookie:
-        del session['credentials']
-
-    if 'doNext' in request.args and request.args['doNext'] == 'login':
-        return redirect(url_for('login'))
-    else:
-        return render_template('logoutSuccess.html')
-
-@app.route('/identity/login/authorize')
-def authorize():
-    # Construct the Flow object:
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-    'client_secret_217930784500-l9noq9hdupkormpjoamplnvsp3078q88.apps.googleusercontent.com.json',
-    scopes = ['profile', 'email'])
-
-    # Set the Redirect URI:
-    flow.redirect_uri = url_for('oauth2callback', _external = True)
-
-    # Generate URL for request to Google's OAuth 2.0 server:
-    authorization_url, state = flow.authorization_url(
-        # Enable offline access so as to be able to refresh an access token withou re-prompting the user for permission
-        access_type = 'offline',
-        # Enable incremental authorization
-        include_granted_scopes = 'true',
-        # Specify the Google Apps domain so that the user can only login using a 'drew.edu' email address.
-        # NOTE: This can be overridden by the user by modifying the request URL in the browser, which is why the login() view  double-checks the domain of the logged-in user's email to ensure it's a 'drew.edu' email address.
-        hd = 'drew.edu'
-        )
-
-    # Store the state so the callback can verify the auth server response:
-    session['state'] = state
-
-    return redirect(authorization_url)
-
-# Process the authorization callback
-@app.route('/identity/login/oauth2callback')
-def oauth2callback():
-    # Specify the state when creating the flow in the callback so that it can verified in the authorization server response:
-    state = session['state']
-
-    # Reconstruct the flow object:
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-    'client_secret_217930784500-l9noq9hdupkormpjoamplnvsp3078q88.apps.googleusercontent.com.json',
-    scopes = ['profile', 'email'],
-    state = state)
-    flow.redirect_uri = url_for('oauth2callback', _external = True)
-
-    # Use the authorization server's response to fetch the OAuth 2.0 tokens:
-    authorization_response = request.url.strip()
-    flow.fetch_token(authorization_response = authorization_response)
-
-    # Store credentials in the session:
-    session['credentials'] = credentials_to_dict(flow.credentials)
-
-    return redirect(url_for('login'))
-
-# Display invalid-sign-in page and prompt for re-login:
-@app.route('/identity/domainInvalid')
-def domainInvalid():
-    return render_template('domainInvalid.html')
-
-
-# HELPER FUNCTIONS
-
-def credentials_to_dict(credentials):
-  return {'token': credentials.token,
-          'refresh_token': credentials.refresh_token,
-          'token_uri': credentials.token_uri,
-          'client_id': credentials.client_id,
-          'client_secret': credentials.client_secret,
-          'scopes': credentials.scopes}
 
 
 if __name__=="__main__":
